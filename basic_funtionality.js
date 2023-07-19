4cc59fad7594681ef88189ab38f51725893bb832
Karl <karlimmanuelholt@tutanota.com>
2023-07-19 20:28:04 +0200

    made the fan menu interactive

--- basic_funtionality.js
+++ basic_funtionality.js
@@ -6,3 +6,53 @@ function select(fan){
 	
 	main.src =  fan.innerHTML.replaceAll(" ", "")+'.html';
 }
+
+fucker = 2;
+big_winow = false;
+function resizing_header() {
+	if(window.innerWidth < 750){
+		document.getElementById("dropdown").style.display = "flex";
+		document.getElementById("fans").style.display = "none";
+		big_winow = false;
+	} else {
+		big_winow = true;
+		delete_menu();
+		document.getElementById("dropdown").style.display = "none";
+		document.getElementById("fans").style.display = "flex";
+	}
+}
+
+function dropdown_menu() {
+	let the_menu = document.getElementById("fans");
+	the_menu.style.display = "block";
+	the_menu.style.position = "absolute";
+	the_menu.style.top = 0;
+	the_menu.style.right = 0;
+	the_menu.style.backgroundColor = "#bbac8d";
+
+	the_menu.style.borderWidth = "2px";
+	the_menu.style.borderLeft = "solid";
+	the_menu.style.borderBottom = "solid";
+	the_menu.style.borderImage = "radial-gradient(rgb(0, 0, 0), rgb(179, 180, 179)) 1";
+	document.getElementById("header").insertBefore(the_menu, document.getElementById("header").childNodes[0]);
+	fucker = 0
+
+	window.focus();
+	window.addEventListener('blur',function(){
+			delete_menu();
+	});
+}
+
+function delete_menu() {
+	if(0 < fucker && !big_winow){
+		let the_menu = document.getElementById("fans");
+		the_menu.style.display = "none";
+		the_menu.style.backgroundColor = "transparent";
+		the_menu.style.border = "0";
+	}
+	fucker++;
+}
+
+window.addEventListener('resize', resizing_header, true);
+
+resizing_header();
